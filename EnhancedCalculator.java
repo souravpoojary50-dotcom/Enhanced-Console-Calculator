2f30c47c159e6bcd908d21f79532edccbb9f34db
Sowrav <souravpoojay50@gmail.com>
2026-03-13 22:16:08 +0530

    Successfully completed

--- EnhancedCalculator.java
+++ EnhancedCalculator.java
@@ -15,7 +15,6 @@ public class EnhancedCalculator {
             System.out.println("6.Power");
             System.out.println("7.Temperature Conversion");
             System.out.println("8.Exit");
-            System.out.println("8.Exit");
             System.out.println("Enter your choice:");
             int choice= sc.nextInt();
             switch (choice) {
@@ -27,8 +26,67 @@ public class EnhancedCalculator {
                     System.out.println("Result:"+(a+b));
                     
                     break;
-            
+                case 2:
+                    System.out.println("Enter first number:");
+                    a=sc.nextDouble();
+                    System.out.println("Enter second number:");
+                    b=sc.nextDouble();
+                    System.out.println("Result:"+ (a-b));
+                    break;
+                case 3:
+                    System.out.println("Enter first number:");    
+                    a=sc.nextDouble();
+                    System.out.println("Enter second number:");
+                    b=sc.nextDouble();
+                    System.out.println("Result:"+ (a*b));
+                    break;
+                case 4:
+                    System.out.println("Enter first number:");    
+                    a=sc.nextDouble();
+                    System.out.println("Enter second number:");
+                    b=sc.nextDouble();
+                    if(b!=0){
+                        System.out.println("Result:"+ (a/b));
+                    } else{
+                        System.out.println("Cannot divide by zero");
+                        }
+                    break;
+                case 5:
+                    System.out.println("Enter number:");    
+                    double num=sc.nextDouble();
+                    System.out.println("Square Root:"+ Math.sqrt(num));
+                    break;
+                case 6:
+                    System.out.println("Enter base:");    
+                    double base=sc.nextDouble();
+                    System.out.println("Enter exponent:");
+                    double power=sc.nextDouble();
+                    System.out.println("Result:"+ Math.pow(base,power));
+                    break;
+                case 7:
+                    System.out.println("1.Celsius to Fahrenhit");    
+                    System.out.println("2.Fahrenheit to Celsius");
+                    int option=sc.nextInt();
+                    if(option==1){
+                         System.out.println("Enter Celcius:");   
+                         double c=sc.nextDouble();
+                         double f=(c*9/5)+32;
+                         System.out.println("Fahrenheit:"+ f);
+                    } else if(option==2){
+                        System.out.println("Enter Fahrenheit:");
+                        double f=sc.nextDouble();
+                        double c=(f-32)*5/9;
+                        System.out.println("Celcius:"+ c);
+                    } 
+                    else {
+                        System.out.println("Invalid option");
+                    }
+                    break;
+                case 8:
+                    System.out.println("Exiting...");
+                    return;    
                 default:
+                    System.out.println("Invalid choice");
                     break;
             }
         }
